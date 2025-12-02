66ba4380be27aa060cb5c67623a708de98b26e0f
James Barnett <jamesdeer66666@gmail.com>
2025-12-02 11:56:03 -0600

    Initial commit - C# Learning Game

---
+++ README.md
@@ -0,0 +1,2 @@
+# C# Learning Game (React + Vite)
+GitHub-ready project. Insert full component code from canvas.
---
+++ src/App.jsx
@@ -0,0 +1,1 @@
+export default function App(){ return (<div className='p-6'><h1 className='text-3xl'>Insert Game Component</h1></div>);}
